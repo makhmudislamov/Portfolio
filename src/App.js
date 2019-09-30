ea8ab755ac02a83fe14a2b573b5db82ffd1c88cc
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-29 22:20:16 -0700

    added Footer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import PageHeader from './PageHeader'
 import PageContent from './PageContent'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <PageHeader />
       <PageContent />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <h2>Makhmud Islamov copyright 2019</h2>
+        </div>
+    )
+}
+
+export default Footer
