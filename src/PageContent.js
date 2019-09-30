5db7824ab230ba12d9ba0d6dcbb3c7bfa028fe14
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-29 21:50:55 -0700

    content component built

--- src/PageContent.js
+++ src/PageContent.js
@@ -4,12 +4,12 @@ import Project from './Project'
 function PageContent() {
     return (
         <div>
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            <Project title="Tetris Dots" image="/images/project2.jpeg" link="#" />
+            <Project title="Zombie Server" image="/images/project.png" link="#" />
+            <Project title="Amazing Colors" image="/images/project2.jpeg" link="#" />
+            <Project title="Flip Toggle" image="/images/project.png" link="#" />
+            <Project title="121 Second St" image="/images/project2.jpeg" link="#" />
+            <Project title="Slide Shows" image="/images/project.png" link="#" />
         </div>
     )
 }
--- src/Project.js
+++ src/Project.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
-function Project() {
+// USING PROPS
+// function Project(props) {
+//     return (
+//         <div>
+//             <img src={props.image} width="300" height="200" />
+//             <h3>{props.image}</h3>
+//             <a href={props.link}>Link to project</a>
+//         </div>
+//     )
+// }
+
+
+// DECONSTRUCTING PROPS
+function Project({ image, title, link }) {
     return (
         <div>
-            <img src='/images/project2.jpeg' width="300" height="200" />
-            <h3>Title of Project</h3>
-            <a href="#">Link to project</a>
+            <img src={image} width="300" height="200" />
+            <h3>{title}</h3>
+            <a href={link}>Link to project</a>
         </div>
     )
 }
-
 export default Project
