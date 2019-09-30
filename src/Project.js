30aab86a1e630a184a92ec4e06c60ed005c51b90
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-29 21:44:04 -0700

    showing project image

--- src/Project.js
+++ src/Project.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Project() {
     return (
         <div>
-            <img src="#" width="300" height="200" />
+            <img src='/images/project2.jpeg' width="300" height="200" />
             <h3>Title of Project</h3>
             <a href="#">Link to project</a>
         </div>
