11fa7cbc6885e5e8698e766e085d935abbee0c10
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-29 21:23:47 -0700

    created PageHeader component

---
+++ src/PageHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageHeader() {
+    return (
+      <div>  
+        <h1>Makhmud Islamov - Portfolio</h1>
+      </div>  
+    )
+}
+
+export default PageHeader
